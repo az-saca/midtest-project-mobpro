9e8e2697ecfbc9c794a4265258aa18aebf24b58a
CHURUN AIN AZKAL AZKIYA <churunain.ra@unipdu.ac.id>
2023-11-05 09:42:07 +0700

    password, speech, openCamera done

---
+++ js/routes.js
@@ -0,0 +1,7 @@
+var routes = [
+  {path: '/', componentUrl: 'pages/home.html'},
+  {path: '/array_online/', componentUrl: 'pages/array.html'},
+  {path: '/data_product/', componentUrl: 'pages/product.html'},
+    {path: '(.*)', url:'pages/404.html'}
+
+]
